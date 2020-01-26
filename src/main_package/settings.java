68d072dbdfa75e962508609f78de0538ae921a2c
Emmanuelamanga <emmanuelamanga2013@gmail.com>
2020-01-26 16:41:59 +0300

    Initial commit. models panels buttons text fields complete

---
+++ src/main_package/settings.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main_package;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author Doreen Midecha
+ */
+public class settings {
+
+    /**
+     *function to set models at the center of the screen
+     */
+    public void dim (){
+//        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+//        this.setLocation(d.width/2 - this.getSize().width/2, d.height/2 - this.getSize().height/2);
+    }
+    
+}
