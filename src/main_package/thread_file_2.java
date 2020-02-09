688bb28dd89f2200a3a28443a1a91948f697a165
Emmanuelamanga <emmanuelamanga2013@gmail.com>
2020-02-09 16:52:25 +0300

    added thread_file_2 a re-usable thread class

---
+++ src/main_package/thread_file_2.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main_package;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Emmanuel Amanga
+ */
+public class thread_file_2 implements Runnable {
+//    variable declarations
+    public JFrame frm, nxtjf;
+    public String[] jlb;
+    private Thread thread;
+    private int sleep_time;
+    
+//    constructors
+    public thread_file_2(JFrame jf,int st,JFrame njf){
+        this.frm = jf;
+        this.sleep_time = st;
+        this.jlb = null;
+        this.nxtjf = njf;
+    }
+    
+    
+
+    @Override
+    public void run() {
+        try {
+//         display the splash
+            frm.setVisible(true);
+//            delay for sleep_time amount of time
+            Thread.sleep(sleep_time);
+//            dispose the splash frame
+            frm.dispose();
+//            display success message
+             JOptionPane.showMessageDialog(null, "Data successfully appended at the end of file", "File", JOptionPane.INFORMATION_MESSAGE);
+//           prompt next JFrame
+                nxtjf.setVisible(true);
+        } catch (InterruptedException e) {
+            JOptionPane.showMessageDialog(null, "!!!\n" + e, "WARNING", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+      /**
+     * start thread
+     */
+    public void start() {
+         if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+    public void stop(){
+        thread.stop();
+    }
+
+}
